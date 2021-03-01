7d6234c11e0f1d4fe4a7300ed2eb246a8b543df3
Thiago Brandão <thiagothgb@gmail.com>
2021-03-01 12:55:18 -0400

    Initial commit

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+import { Container } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <Container></Container>
+    </>
+  );
+};
+
+export default Dashboard;
